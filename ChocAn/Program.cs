5a04e5028dc61a27121176380c52eeb0b0def7f3
matthew17754 <matmoradi17@gmail.com>
2022-01-14 13:10:26 -0800

    Setup Initial Interface

---
+++ ChocAn/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChocAn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ChocAn Terminal v1.0\n");
+            Console.WriteLine("--- Select Your Role: ---\n");
+            Console.WriteLine("1) \n");
+        }
+    }
+}
