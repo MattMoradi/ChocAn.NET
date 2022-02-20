062d5c9c04c103b5103b6fdd9553383dbe05c50e
matthew17754 <matmoradi17@gmail.com>
2022-02-20 10:05:11 -0800

    Implemented Disk Write Utility

--- Database.cs
+++ Database.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Text;
 
 namespace ChocAn
 {
@@ -8,6 +8,86 @@ namespace ChocAn
     {
         public Members[] members = new Members[999];
         public Providers[] providers = new Providers[999];
+
+        public Database()
+        {
+            try
+            {
+                // Save Provider Directory File
+                StreamWriter pd = new StreamWriter("ProviderDirectory.txt");
+                pd.WriteLine(ProviderDirectory.Aerobics + " - " + (int)ProviderDirectory.Aerobics);
+                pd.WriteLine(ProviderDirectory.Dietitian + " - " + (int)ProviderDirectory.Dietitian);
+                pd.WriteLine(ProviderDirectory.Vanilla + " - " + (int)ProviderDirectory.Vanilla);
+                pd.WriteLine(ProviderDirectory.Hamster + " - " + (int)ProviderDirectory.Hamster);
+                pd.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+
+        public void save2disk(Database data)
+        {
+            StreamWriter Writer;
+
+            try
+            {
+                Directory.CreateDirectory("Members");
+
+                for (int i = 0; data.members[i].name != null; i++)
+                {
+                    //Console.WriteLine("Member" + Path.DirectorySeparatorChar + $"{data.members[i].name}.txt"); //delete
+                    Writer = new StreamWriter("Members" + Path.DirectorySeparatorChar + $"{data.members[i].name}.txt");
+                    Writer.WriteLine(data.members[i].name);
+                    Writer.WriteLine(data.members[i].number);
+                    Writer.WriteLine(data.members[i].address);
+                    Writer.WriteLine(data.members[i].city);
+                    Writer.WriteLine(data.members[i].state);
+                    Writer.WriteLine(data.members[i].zip);
+                    Writer.WriteLine(data.members[i].status);
+
+                    for (int j = 0; data.members[i].records[j].date != null; j++)
+                    {
+                        Writer.WriteLine(data.members[i].records[j].date);
+                        Writer.WriteLine(data.members[i].records[j].providerName);
+                        Writer.WriteLine(data.members[i].records[j].service);
+                    }
+                    Writer.Close();
+                }
+
+                Directory.CreateDirectory("Providers");
+
+                for (int i = 0; data.providers[i].name != null; i++)
+                {
+                    Writer = new StreamWriter("Providers" + Path.DirectorySeparatorChar + $"{data.providers[i].name}.txt");
+                    Writer.WriteLine(data.providers[i].name);
+                    Writer.WriteLine(data.providers[i].number);
+                    Writer.WriteLine(data.providers[i].address);
+                    Writer.WriteLine(data.providers[i].city);
+                    Writer.WriteLine(data.providers[i].state);
+                    Writer.WriteLine(data.providers[i].zip);
+                    Writer.WriteLine(data.providers[i].consultations);
+                    Writer.WriteLine(data.providers[i].totalFee);
+
+                    for (int j = 0; data.providers[i].records[j].date != null; j++)
+                    {
+                        Writer.WriteLine(data.providers[i].records[j].date);
+                        Writer.WriteLine(data.providers[i].records[j].timestamp);
+                        Writer.WriteLine(data.providers[i].records[j].memberName);
+                        Writer.WriteLine(data.providers[i].records[j].memberNumber);
+                        Writer.WriteLine(data.providers[i].records[j].serviceCode);
+                        Writer.WriteLine(data.providers[i].records[j].fee);
+                        Writer.WriteLine(data.providers[i].records[j].comment);
+                    }
+                    Writer.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
 
         public struct Members
         {
--- Provider.cs
+++ Provider.cs
@@ -11,6 +11,7 @@ namespace ChocAn
             Console.Clear();
             Console.WriteLine("ChocAn Terminal v1.0\n");
             Console.WriteLine("Hello Provider!");
+
             Console.WriteLine("Press any key to return to main menu...");
             Console.ReadKey();
         }
