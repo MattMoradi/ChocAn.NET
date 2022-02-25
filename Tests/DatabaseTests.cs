f3235f215c074a8b60345a331d47b8343875b1ba
matthew17754 <matmoradi17@gmail.com>
2022-02-24 19:43:46 -0800

    Added Database Disk Write Tests

--- Tests/DatabaseTests.cs
+++ Tests/DatabaseTests.cs
@@ -1,14 +1,45 @@
+using ChocAn;
 using System;
+using System.IO;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Tests
 {
     public class DatabaseTests
     {
-        [Fact]
-        public void Test1()
+        private readonly ITestOutputHelper Output;
+        private readonly char slash;
+        private readonly Database database;
+
+        public DatabaseTests(ITestOutputHelper Output)
         {
-            Assert.True(true);
+            this.Output = Output;
+            slash = Path.DirectorySeparatorChar;
+            database = new Database();
+        }
+
+        [Fact]
+        public void ProviderDirectoryTest()
+        {
+
+            Assert.True(File.Exists("ProviderDirectory.txt"));
+            Output.WriteLine(Environment.CurrentDirectory + slash + "ProviderDirectory.txt");
+        }
+
+        [Fact]
+        public void EFTTest()
+        {
+            database.writeEFT(database);
+            Assert.True(File.Exists("EFT.txt"));
+        }
+
+        [Fact]
+        public void DBSaveTest()
+        {
+            database.save2disk(database);
+            Assert.True(Directory.Exists("Members"));
+            Assert.True(Directory.Exists("Providers"));
         }
     }
 }
