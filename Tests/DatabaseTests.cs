6bf0e2a560ae8fbfd115596d11b382ca3bf4a2be
Moha29 <81183253+Moha29@users.noreply.github.com>
2022-03-09 22:53:48 -0800

    Implemented the member terminal and unit tests

--- Tests/DatabaseTests.cs
+++ Tests/DatabaseTests.cs
@@ -4,6 +4,7 @@ using System.IO;
 using Xunit;
 using Xunit.Abstractions;
 
+
 namespace Tests
 {
     public class DatabaseTests
@@ -40,6 +41,35 @@ namespace Tests
             database.save2disk(database);
             Assert.True(Directory.Exists("Members"));
             Assert.True(Directory.Exists("Providers"));
+        }
+    }
+    public class Memberstest
+    {
+        private readonly Database.Members[] members = new Database.Members[999];
+
+        [Fact]
+        public void Validtest1()
+        {
+            members[0].status = (Database.Validity)0;
+            int expected = 1;
+            int actual = Member.is_valid(members, 0);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Validtest2()
+        {
+            members[3].status = (Database.Validity)2;
+            int expected = 2;
+            int actual = Member.is_valid(members, 3);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Validtest3()
+        {
+            members[0].status = (Database.Validity)1;
+            int expected = 3;
+            int actual = Member.is_valid(members, 0);
+            Assert.Equal(expected, actual);
         }
     }
 }
