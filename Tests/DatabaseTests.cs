300ba671d60edf731a0a845797583484eaf3163a
Moha29 <81183253+Moha29@users.noreply.github.com>
2022-03-10 00:30:33 -0800

    Member Final

--- Tests/DatabaseTests.cs
+++ Tests/DatabaseTests.cs
@@ -7,42 +7,6 @@ using Xunit.Abstractions;
 
 namespace Tests
 {
-    public class DatabaseTests
-    {
-        private readonly ITestOutputHelper Output;
-        private readonly char slash;
-        private readonly Database database;
-
-        public DatabaseTests(ITestOutputHelper Output)
-        {
-            this.Output = Output;
-            slash = Path.DirectorySeparatorChar;
-            database = new Database();
-        }
-
-        [Fact]
-        public void ProviderDirectoryTest()
-        {
-
-            Assert.True(File.Exists("ProviderDirectory.txt"));
-            Output.WriteLine(Environment.CurrentDirectory + slash + "ProviderDirectory.txt");
-        }
-
-        [Fact]
-        public void EFTTest()
-        {
-            database.writeEFT(database);
-            Assert.True(File.Exists("EFT.txt"));
-        }
-
-        [Fact]
-        public void DBSaveTest()
-        {
-            database.save2disk(database);
-            Assert.True(Directory.Exists("Members"));
-            Assert.True(Directory.Exists("Providers"));
-        }
-    }
     public class Memberstest
     {
         private readonly Database.Members[] members = new Database.Members[999];
---
+++ Tests/MemberTests.cs
@@ -0,0 +1,47 @@
+using ChocAn;
+using System;
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace Tests
+{
+    public class DatabaseTests
+    {
+        private readonly ITestOutputHelper Output;
+        private readonly char slash;
+        private readonly Database database;
+
+        public DatabaseTests(ITestOutputHelper Output)
+        {
+            this.Output = Output;
+            slash = Path.DirectorySeparatorChar;
+            database = new Database();
+        }
+
+        [Fact]
+        public void ProviderDirectoryTest()
+        {
+
+            Assert.True(File.Exists("ProviderDirectory.txt"));
+            Output.WriteLine(Environment.CurrentDirectory + slash + "ProviderDirectory.txt");
+        }
+
+        [Fact]
+        public void EFTTest()
+        {
+            database.writeEFT(database);
+            Assert.True(File.Exists("EFT.txt"));
+        }
+
+        [Fact]
+        public void DBSaveTest()
+        {
+            database.save2disk(database);
+            Assert.True(Directory.Exists("Members"));
+            Assert.True(Directory.Exists("Providers"));
+        }
+    }
+   
+}
