b6c0ed7b981a11ac2e77981743e0bba5c70f3db2
Cesium1289 <Carson.hansen@gmail.com>
2022-03-10 17:24:28 -0800

    Added more Operator unit tests
    Added more unit tests for the operator terminal

--- Tests/OperatorTests.cs
+++ Tests/OperatorTests.cs
@@ -1,6 +1,7 @@
 using ChocAn;
 using System;
 using System.IO;
+using System.Linq;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -11,12 +12,43 @@ namespace Tests
         private readonly ITestOutputHelper Output;
         private readonly Database database;
        
-
         public OperatorTests(ITestOutputHelper Output)
         {
             this.Output = Output;
             database = new Database();
            
+        }
+        [Fact]
+        public void RemoveMemberTest()
+        {
+            string name = "Nancy Principato";
+            int number = 364702985;
+            string address = "1054 Pinewood Drive";
+            string city = " Arlington Heights";
+            string state = "Illionois";
+            int zip = 60004;
+
+            Operator.CreateMember(database, name, address, city, state, zip, number);
+            Assert.True(database.members[0].name != null, "The member was not added to the database");
+            Operator.RemoveMember(database, database.members[0].number);
+            Assert.True(database.members[0].name == null,"The member was not removed from the database");
+        }
+
+        
+        [Fact]
+        public void RemoveProviderTest()
+        {
+            string name = "John Goodman";
+            int number = 154782046;
+            string address = "123 ne fake st";
+            string city = "New City";
+            string state = "Ohio";
+            int zip = 12345;
+
+            Operator.CreateProvider(database, name, address, city, state, zip, number);
+            Assert.True(database.providers[0].name != null, "The provider was not added to the database");
+            Operator.RemvoveProvider(database, database.providers[0].number);
+            Assert.True(database.providers[0].name == null, "The provider was not removed from the database");
         }
         [Fact]
         public void CreateMemberTest()
@@ -65,6 +97,19 @@ namespace Tests
             int intSelection;
             int numChoices = 3;
             Assert.True(Operator.ValidInput(stringInt, out intSelection, numChoices));
+        }
+
+        [Fact]
+        public void GenerateIDTest()
+        {
+            for(int i =0; i < 999;i++)
+                database.members[i].number = Operator.GenerateID(database, "Member");
+
+            for(int i =0; i <999;i++)
+             database.providers[i].number = Operator.GenerateID(database, "Provider");
+
+            Assert.True(database.members.Count() == database.members.Distinct().Count(),"There was a duplicate number found in the member generate id");
+            Assert.True(database.providers.Count() == database.providers.Distinct().Count(), "There was a duplicate number found in the providers generate id");
         }
     }
 }
